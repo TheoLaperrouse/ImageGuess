a90da7c158f302c6f938386669edf8053f73ef40
Théo Laperrouse <theo.laperrouse@energiency.com>
2023-10-04 15:20:40 +0200

    feat: Better notification and add a fullscreen mode

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="bg-blue-500 p-4">
+    <nav class="bg-blue-500 p-4 flex items-center" :class="{ hidden: isFullScreen }">
         <a
             href="https://github.com/TheoLaperrouse/CelebrityGuess"
             target="_blank"
@@ -8,6 +8,10 @@
         >
             <FontAwesomeIcon :icon="['fab', 'github']" /> GitHub
         </a>
+        <div class="flex-grow"></div>
+        <button @click="toggleFullScreen()" class="text-white mr-2">
+            <FontAwesomeIcon :icon="['fas', 'expand']" />
+        </button>
     </nav>
 </template>
 
@@ -17,6 +21,32 @@ import FontAwesomeIcon from '../fontawesome';
 export default {
     components: {
         FontAwesomeIcon,
+    },
+    data() {
+        return {
+            isFullScreen: false,
+        };
+    },
+    created() {
+        const storedValue = localStorage.getItem('fullscreenPreference');
+        this.isFullScreen = storedValue ? JSON.parse(storedValue) : false;
+    },
+    mounted() {
+        document.addEventListener('keydown', this.handleKeyPress);
+    },
+    beforeUnmount() {
+        document.removeEventListener('keydown', this.handleKeyPress);
+    },
+    methods: {
+        toggleFullScreen(bool = true) {
+            this.isFullScreen = bool;
+            localStorage.setItem('fullscreenPreference', this.isFullScreen);
+        },
+        handleKeyPress(event) {
+            if (event.key === 'Escape' && this.isFullScreen) {
+                this.toggleFullScreen(false);
+            }
+        },
     },
 };
 </script>
--- src/views/GamePage.vue
+++ src/views/GamePage.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="bg-gray-900 text-white flex flex-col items-center justify-center">
+    <div class="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center">
         <div class="mb-6">
             <span class="text-2xl font-semibold text-blue-500">Temps restant : {{ remainingTime }} secondes</span>
             <span class="ml-4 text-2xl font-semibold">Score : {{ score }}</span>
         </div>
         <div class="picture-container flex-1 flex items-center justify-center">
-            <picture-input :image-url="imageUrl" @guess-validated="guessCelebrity" />
+            <picture-input :image-url="celebrity.url" @guess-validated="guessCelebrity" />
         </div>
     </div>
 </template>
@@ -14,6 +14,7 @@
 import { distance } from 'fastest-levenshtein';
 import PictureInput from '../components/PictureInput.vue';
 import celebritiesInfos from '@/assets/celebritiesInfos.json';
+import { Toast } from '@/components/toast.js';
 import Swal from 'sweetalert2';
 
 const ACCEPTABLE_DIST = 2;
@@ -28,6 +29,7 @@ export default {
             celebrity: {},
             remainingTime: 60,
             score: 0,
+            intervalId: null,
         };
     },
     created() {
@@ -41,34 +43,36 @@ export default {
             }
         }, 1000);
     },
+    beforeUnmount() {
+        clearInterval(this.intervalId);
+    },
     methods: {
         swapCelebrity() {
             if (this.celebritiesInfos.length === 0) {
-                this.endGame('Vous avez trouvé toutes les célébrités. Bravo\n');
+                this.endGame('Vous avez trouvé toutes les célébrités.');
                 return;
             }
-
             const index = Math.floor(Math.random() * this.celebritiesInfos.length);
-            this.imageUrl = this.celebritiesInfos[index].url;
-            this.celibrity = this.celebritiesInfos[index];
+            this.celebrity = this.celebritiesInfos[index];
             this.celebritiesInfos.splice(index, 1);
         },
         endGame(message) {
-            Swal.fire(`${message ?? ''}Vous avez eu un score de ${this.score}`);
+            Swal.fire(`${message ?? ''}Bravo, vous avez eu un score de ${this.score}`);
             clearInterval(this.intervalId);
             this.$router.push('/');
         },
         guessCelebrity(name) {
-            if (distance(name, this.celibrity.name) <= ACCEPTABLE_DIST) {
+            if (distance(name, this.celebrity.name) <= ACCEPTABLE_DIST) {
                 this.score += 1;
+                Toast.fire({
+                    icon: 'success',
+                    title: 'Bravo, +1 point',
+                });
                 this.swapCelebrity();
             } else {
-                Swal.fire({
+                Toast.fire({
                     icon: 'error',
-                    title: 'FAUX',
-                    text: "Ce n'est pas la bonne réponse",
-                    timer: 1000,
-                    showConfirmButton: false,
+                    title: "Ce n'est pas son nom",
                 });
             }
         },
