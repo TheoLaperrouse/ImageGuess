8e44255f63869e95350572c0fd1866180a395e3f
Théo Laperrouse <theo.laperrouse@energiency.com>
2023-10-04 15:18:57 +0200

    fix: Add FontAwesome icons

--- src/fontawesome.js
+++ src/fontawesome.js
@@ -2,10 +2,12 @@
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faExpand } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 // Ajoutez les icônes à la bibliothèque FontAwesome
 library.add(faGithub);
+library.add(faExpand);
 
 // Exportez le composant FontAwesomeIcon pour une utilisation dans d'autres fichiers
 export default FontAwesomeIcon;
