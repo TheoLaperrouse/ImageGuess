4c770c6adfece6c9d1e84f5e0d7eec403e97b997
Théo Laperrouse <theo.laperrouse@energiency.com>
2023-10-07 14:54:58 +0200

    fix: Harmonize design

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <div class="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center">
         <h1 class="text-2xl mb-5">Bienvenue au jeu de reconnaissance de célébrité !</h1>
         <p class="text-base mb-5">On cherche le nom de famille de la célébrité</p>
         <button @click="beginGame" class="px-4 py-2 text-white bg-blue-500 rounded-lg transition hover:bg-blue-700">
@@ -7,7 +7,6 @@
         </button>
     </div>
 </template>
-
 <script>
 export default {
     methods: {
